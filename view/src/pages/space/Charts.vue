0316d10045d2a8d5738d241f258a957db678fde0
xihuyu20000-admin <xihuyu2000@gmail.com>
2025-01-07 00:49:37 +0800

    完善

--- view/src/pages/space/Charts.vue
+++ view/src/pages/space/Charts.vue
@@ -2,9 +2,7 @@
   <div>
     <vxe-table ref="xTable" border highlight-current-row show-overflow :data="tableData">
       <vxe-column type="seq" width="50"></vxe-column>
-      <vxe-column field="rawname" title="文件名称" min-width="100"></vxe-column>
-      <vxe-column field="fpath" title="文件路径" min-width="200"></vxe-column>
-      <vxe-column field="project_name" title="所属项目" min-width="200"></vxe-column>
+      <vxe-column field="config.title" title="图表名称" min-width="100"></vxe-column>
       <vxe-column field="active" title="按钮" width="200">
         <template #default="{ row }">
           <vxe-button mode="text" status="success" @click="previewIns(row)">预览</vxe-button>
@@ -30,17 +28,14 @@ const tableData = ref([]);
 const previewIns = (row) => {};
 
 const editIns = (row) => {
-  // 4-1 字符串转json
-  const config = JSON.parse(row["config"]);
-  const option = JSON.parse(row["option"]);
-  // 4-2 跳转到编辑器页面
-  router.push(config.chart_id);
-  // 4-3 一定要更新全局变量
-  globalStore.ins_id = config["ins_id"];
-  // 4-4 延迟更新全局变量，这样编辑器会更新数据
+  // 4-1 跳转到编辑器页面
+  router.push(row.config.chart_id);
+  // 4-2 一定要更新全局变量
+  globalStore.ins_id = row.config.ins_id;
+  // 4-3 延迟更新全局变量，这样编辑器会更新数据
   setTimeout(() => {
-    globalStore.setConfig(reactive(config));
-    globalStore.setOption(reactive(option));
+    globalStore.setConfig(reactive(row.config));
+    globalStore.setOption(reactive(row.option));
     console.log("更新globalStore");
     console.log(globalStore.config);
     console.log(globalStore.option);
@@ -54,7 +49,13 @@ onMounted(async () => {
   });
   if (resp.code === 200) {
     console.log(resp.data);
-    tableData.value = resp.data;
+    tableData.value = resp.data.map((item, i, arr) => {
+      return {
+        ins_id: item.ins_id,
+        config: JSON.parse(item.config),
+        option: JSON.parse(item.option),
+      };
+    });
   }
 });
 </script>
